a317597fef9afe624e473f5c36f2e41660bd283e
Andy Wang <wang.andy32@gmail.com>
2018-04-11 12:21:43 -0400

    added modal on front page

--- Javascript/app.js
+++ Javascript/app.js
@@ -8,6 +8,30 @@ menu.addEventListener("click", function(){
 		navLists[i].classList.toggle("navListOn");
 	}
 });
+///////////////////////////////////////////////////////////////
+// Modal
+var modal = document.querySelector(".modal");
+var closeBtn = document.querySelector(".close");
+
+window.addEventListener("load",function(){
+      setTimeout(delayedModal,1500);
+});
+
+function delayedModal(){
+	modal.classList.toggle("navListOn");
+}
+
+closeBtn.addEventListener("click", function(){
+	modal.classList.toggle("navListOn");
+});
+
+
+
+
+
+
+
+
 ///////////////////////////////////////////////////////////////////////
  // Initialize Firebase
   var config = {
